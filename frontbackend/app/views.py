09182e8700ebba76c5323969904efa09e863221f
Patrick Bope <patbpm1@gmail.com>
2020-04-12 16:59:09 +0200

    Setting up the urls, views and the homepage.html files

--- frontbackend/app/views.py
+++ frontbackend/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage(request):
+    
+    context = {
+        
+    } 
+    
+    return render(request, 'app/templates/homepage.html', context)
