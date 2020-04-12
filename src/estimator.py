c227816e5b4af298f5c6458640f88dc84eb4455f
Patrick Bope <patbpm1@gmail.com>
2020-04-12 08:42:35 +0200

    Estimation calculation for the second challenge

--- src/estimator.py
+++ src/estimator.py
@@ -6,20 +6,32 @@ def estimator(data):
   reportedCases = data['reportedCases']
   periodType = data['periodType']
   timeToElapse = data['timeToElapse']
+  totalHospitalBeds = data['totalHospitalBeds']
 
-  
-  #CHALLENGE 1
-
+  # Conversion of the duration of the time elapse in day
   duration = get_duration_in_day(periodType, timeToElapse)
   factor = duration//3
 
-  #For Impact
+  #Result for Impact
+  #CHALLENGE 1
   impact['currentlyInfected'] = reportedCases * 10
   impact['infectionsByRequestedTime'] = impact['currentlyInfected'] * (2**factor)
 
-  #For Severe Impact
+  #CHALLENGE 2
+  impact['severeCasesByRequestedTime'] = int(impact['infectionsByRequestedTime'] * 0.15) 
+  impact['hospitalBedsByRequestedTime'] = number_beds_avail(totalHospitalBeds, impact['severeCasesByRequestedTime'])
+
+
+  #--------------------------------------------------------------------------------------------------
+  #Result for Severe Impact
+  #CHALLENGE 1
   severeImpact['currentlyInfected'] = reportedCases * 50
   severeImpact['infectionsByRequestedTime'] = severeImpact['currentlyInfected']*(2**factor)
+  
+  #CHALLENGE 2
+  severeImpact['severeCasesByRequestedTime'] = int(severeImpact['infectionsByRequestedTime'] * 0.15)
+  severeImpact['hospitalBedsByRequestedTime'] = number_beds_avail(totalHospitalBeds, severeImpact['severeCasesByRequestedTime'])
+
 
 
   result = {'data':data, 'impact':impact, 'severeImpact': severeImpact}
@@ -39,4 +51,25 @@ def get_duration_in_day(periodType, timeToElapse):
         return 0
 
 
+def number_beds_avail(totalHospitalBeds, severeImpactCases):
+  """ calculating the number of beds available as integers and not floats"""
+  avail_beds = int((0.35 * totalHospitalBeds) - severeImpactCases)
+  
+  return avail_beds
 
+
+data = {
+    "region": {
+        "name": 'Africa',
+        "avgAge": 19.7,
+        "avgDailyIncomeInUSD": 1,
+        "avgDailyIncomePopulation": 0.73
+    },
+    "periodType": 'months',
+    "timeToElapse": 2,
+    "reportedCases": 445,
+    "population": 4578803,
+    "totalHospitalBeds": 172637
+}
+
+print(estimator(data))
