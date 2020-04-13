2a19a4ecd02960df044c815da656b254588bf65a
Patrick Bope <patbpm1@gmail.com>
2020-04-13 02:35:41 +0200

    Fi bugs

--- frontbackend/frontbackend/settings.py
+++ frontbackend/frontbackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '%gb_om&%gzpx(e9+r8$e1s#=)%tjhuaz6u9e0&-oh4vpf3tk=w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
